37627d6ecd4914a5f7b7619615481a6cd76b71a0
John Goerzen <jgoerzen@complete.org>
2019-10-29 00:04:04 -0500

    checkpointing

--- src/lorastik.rs
+++ src/lorastik.rs
@@ -24,14 +24,32 @@ use std::io;
 use crossbeam_channel;
 use hex;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::ascii::escape_default;
 use format_escape_default::format_escape_default;
+
+/** The amount of time to pause before transmitting a packet.  The
+main purpose of this is to give the othe rradio a chance to finish
+decoding the previous packet, send it to the OS, and re-enter RX mode.
+A secondary purpose is to give the duplex logic a chance to see if
+anything else is coming in.  Given in ms.
+*/
+const WAIT_BEFORE_TX_MILLIS: u64 = 50;
+
+/** The amount of time to wait before transmitting after receiving a
+packet that indicated more data was forthcoming.  The purpose of this is
+to compensate for a situation in which the "last" incoming packet was lost,
+to prevent the receiver from waiting forever for more packets before
+transmitting.  Given in ms. */
+const TX_PREVENTION_TIMEOUT_MILLIS: u64 = 1000;
 
 pub fn mkerror(msg: &str) -> Error {
     Error::new(ErrorKind::Other, msg)
 }
 
+/// Received frames.  The option is populated only if
+/// readqual is true, and reflects the SNR and RSSI of the
+/// received packet.
 #[derive(Clone, Debug, PartialEq)]
 pub struct ReceivedFrames(pub Vec<u8>, pub Option<(String, String)>);
 
@@ -49,11 +67,24 @@ pub struct LoraStik {
     txblockstx: crossbeam_channel::Sender<Vec<u8>>,
     txblocksrx: crossbeam_channel::Receiver<Vec<u8>>,
 
-    // Whether or not to read quality blocks
+    // Whether or not to read quality data from the radio
     readqual: bool,
+
+    // Whether we must delay before transmit.  The Instant
+    // reflects the moment when the delay should end.
+    txdelay: Option<Instant>,
+
+    // The wait before transmitting.  Initialized from
+    // [`WAIT_BEFORE_TX_MILLIS`].
+    wait_before_tx: Duration,
+    
+    // The transmit prevention timeout.  Initialized from
+    // [`TX_PREVENTION_TIMEOUT_MILLIS`].
+    tx_prevention_timeout: Duration,
 }
 
-/// Reads the lines from the radio and sends them down the channel.
+/// Reads the lines from the radio and sends them down the channel to
+/// the processing bits.
 fn readerlinesthread(mut ser: LoraSer, tx: crossbeam_channel::Sender<String>) {
     loop {
         let line = ser.readln().expect("Error reading line");
@@ -92,7 +123,10 @@ impl LoraStik {
         
         thread::spawn(move || readerlinesthread(ser2, readerlinestx));
         
-        (LoraStik { readqual, ser, readeroutput, readerlinesrx, txblockstx, txblocksrx}, readeroutputreader)
+        (LoraStik { readqual, ser, readeroutput, readerlinesrx, txblockstx, txblocksrx,
+                    txdelay: None,
+                    wait_before_tx: Duration::from_millis(WAIT_BEFORE_TX_MILLIS),
+                    tx_prevention_timeout: Duration::from_millis(TX_PREVENTION_TIMEOUT_MILLIS)}, readeroutputreader)
     }
 
     /// Utility to read the response from initialization
@@ -122,13 +156,17 @@ impl LoraStik {
 
     /// Utililty function to handle actual sending.  Assumes radio is idle.
     fn dosend(&mut self, data: Vec<u8>) -> io::Result<()> {
-        // Now, send the mesage.
-        let mut txstr = String::from("radio tx ");
-        let hexstr = hex::encode(data);
-        txstr.push_str(&hexstr);
-
         // Give receiver a change to process.
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(self.wait_before_tx);
+
+        let mut flag: u8 = 0;
+        if !self.txblocksrx.is_empty() {
+            flag = 1;
+        }
+
+        // Now, send the mesage.
+        let txstr = format!("radio tx {}{}", hex::encode([flag]), hex::encode(data));
+
         self.ser.writeln(txstr)?;
         
         // We get two responses from this.... though sometimes a lingering radio_err also.
@@ -144,15 +182,29 @@ impl LoraStik {
         Ok(())
     }
 
+    // Receive a message from the incoming radio channel and process it.
     fn handlerx(&mut self, msg: String, readqual: bool) -> io::Result<()> {
         if msg.starts_with("radio_rx ") {
             if let Ok(decoded) = hex::decode(&msg.as_bytes()[10..]) {
                 trace!("DECODED: {}", format_escape_default(&decoded));
-                if readqual {
+                let radioqual = if readqual {
                     self.ser.writeln(String::from("radio get snr"));
                     let snr = self.readerlinesrx.recv().unwrap();
                     self.ser.writeln(String::from("radio get rssi"));
                     let rssi = self.readerlinesrx.recv().unwrap();
+                    Some((snr, rssi))
+                } else {
+                    None
+                }
+
+                if decoded[0] == 1 {
+                    // More data is coming
+                    self.txdelay = Some(Instant::now() + self.tx_prevention_timeout);
+                } else {
+                    self.txdelay = None;
+                }
+
+                if readqual {
                     self.readeroutput.send(ReceivedFrames(decoded, Some((snr, rssi)))).unwrap();
                 } else {
                     self.readeroutput.send(ReceivedFrames(decoded, None)).unwrap();
@@ -165,38 +217,103 @@ impl LoraStik {
         // Might get radio_err here.  That's harmless.
         Ok(())
     }
+
+    // Whether or not a txdelay prevents transmit at this time.  None if
+    // we are cleared to transmit; Some(Duration) gives the amount of time
+    // we'd have to wait otherwise.
+    fn txdelayrequired(&mut self) -> Option<Duration> {
+        match self.txdelay {
+            None => None,
+            Some(delayend) => {
+                let now = Instant::now();
+                if now >= delayend {
+                    // We're past the delay.  Clear it and return.
+                    self.txdelay = None;
+                    None
+                } else {
+                    // Indicate we're still blocked.
+                    Some(delayend - now)
+                }
+            }
+        }
+    }
+
+    fn enterrxmode(&mut self) -> io::Result<()> {
+        // Enter read mode
+        
+        self.ser.writeln(String::from("radio rx 0"))?;
+        let mut response = self.readerlinesrx.recv().unwrap();
+        
+        // For some reason, sometimes we get a radio_err here, then an OK.  Ignore it.
+        if response == String::from("radio_err") {
+            response = self.readerlinesrx.recv().unwrap();
+        }
+        assert_response(response, String::from("ok"))?;
+    }
+
+    fn rxstop(&mut self) -> io::Result<()> {
+        self.ser.writeln(String::from("radio rxstop"))?;
+        let mut checkresp = self.readerlinesrx.recv().unwrap();
+        if checkresp.starts_with("radio_rx ") {
+            // We had a race.  A packet was coming in.  Decode and deal with it,
+            // then look for the 'ok' from rxstop.  We can't try to read the quality in
+            // this scenario.
+            self.handlerx(checkresp, false)?;
+            checkresp = self.readerlinesrx.recv().unwrap();
+        }
+        
+        // Now, checkresp should hold 'ok'.
+        //  It might not be; I sometimes see radio_err here.  it's OK too.
+        // assert_response(checkresp, String::from("ok"))?;
+    }
     
     pub fn readerthread(&mut self) -> io::Result<()> {
         loop {
-            // Do we have anything to send?  Check at the top and keep checking
-            // here so we send as much as possible before going back into read
-            // mode.
-            let r = self.txblocksrx.try_recv();
-            match r {
-                Ok(data) => {
-                    self.dosend(data)?;
-                    continue;
-                },
-                Err(e) => {
-                    if e.is_disconnected() {
-                        // other threads crashed
-                        r.unwrap();
+            // First, check to see if we're allowed to transmit.  If not, just
+            // try to read and ignore all else.
+            if let Some(delayamt) = self.txdelayrequired() {
+                // We can't transmit yet.  Just read, but with a time box.
+                self.enterrxmode();
+                let res = self.readerlinesrx.recv_timeout(delayamt);
+                match res {
+                    Some(msg) => {
+                        self.handlerx(msg)?;
+                        continue;
+                    },
+                    Err(e) => {
+                        if e.is_timeout() {
+                            self.txdelay = None;
+                            // Now we can fall through to the rest of the logic - already in read mode.
+                        } else {
+                            res.unwrap(); // disconnected - crash
+                        }
                     }
-                    // Otherwise - nothing to write, go on through.
                 }
+            } else {
+                // We are allowed to transmit.
+                
+                // Do we have anything to send?  Check at the top and keep checking
+                // here so we send as much as possible before going back into read
+                // mode.
+                let r = self.txblocksrx.try_recv();
+                match r {
+                    Ok(data) => {
+                        self.dosend(data)?;
+                        continue;
+                    },
+                    Err(e) => {
+                        if e.is_disconnected() {
+                            // other threads crashed
+                            r.unwrap();
+                        }
+                        // Otherwise - nothing to write, go on through.
+                    }
+                }
+
+                self.enterrxmode()?;
             }
 
-            // Enter read mode
-            
-            self.ser.writeln(String::from("radio rx 0"))?;
-            let mut response = self.readerlinesrx.recv().unwrap();
-
-            // For some reason, sometimes we get a radio_err here, then an OK.  Ignore it.
-            if response == String::from("radio_err") {
-                response = self.readerlinesrx.recv().unwrap();
-            }
-            assert_response(response, String::from("ok"))?;
-
+            // At this point, we're in rx mode with no timeout.
             // Now we wait for either a write request or data.
 
             let mut sel = crossbeam_channel::Select::new();
@@ -212,19 +329,7 @@ impl LoraStik {
                     // We have something to send.  Stop the receiver and then go
                     // back to the top of the loop to handle it.
 
-                    self.ser.writeln(String::from("radio rxstop"))?;
-                    let mut checkresp = self.readerlinesrx.recv().unwrap();
-                    if checkresp.starts_with("radio_rx ") {
-                        // We had a race.  A packet was coming in.  Decode and deal with it,
-                        // then look for the 'ok' from rxstop.  We can't try to read the quality in
-                        // this scenario.
-                        self.handlerx(checkresp, false)?;
-                        checkresp = self.readerlinesrx.recv().unwrap();
-                    }
-                    
-                    // Now, checkresp should hold 'ok'.
-                    //  It might not be; I sometimes see radio_err here.  it's OK too.
-                    // assert_response(checkresp, String::from("ok"))?;
+                    self.rxstop()?;
                     
                 },
                 _ => panic!("Invalid response from sel.ready()"),
